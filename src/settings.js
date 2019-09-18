ea7c64457ca9b7f1111eb2ec8f8b817c55198f0c
paulll <paulll@derpy.ru>
2019-09-19 02:28:53 +0300

    bump version because of breaking change in database file format

--- src/settings.js
+++ src/settings.js
@@ -2,10 +2,8 @@ const path = require('path');
 
 module.exports = {
 	redis_url: 'redis://:UWfQuFUq6YKxyrNqGHKuCRovhAWma3AD@redis-11760.c135.eu-central-1-1.ec2.cloud.redislabs.com:11760',
-	redis_key: 'gml:current_user',
+	redis_key: 'gml:current_user:v2',
 	output: path.join(__dirname, '../output'),
 	last_block_file: path.join(__dirname, '../.last_block'),
 	groups_per_block: 250,
-	fibers: 20,
-	//proxies: []
 };
