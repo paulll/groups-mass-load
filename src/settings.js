97ac1424bccd485616be8c7edbdec9a88e936bd7
paulll <paulll@derpy.ru>
2019-09-18 19:31:33 +0300

    move last block info to project root

--- src/settings.js
+++ src/settings.js
@@ -4,7 +4,7 @@ module.exports = {
 	redis_url: 'redis://localhost',
 	redis_key: 'gml:current_user',
 	output: path.join(__dirname, '../output'),
-	last_block_file: path.join(__dirname, './.last_block'),
+	last_block_file: path.join(__dirname, '../.last_block'),
 	groups_per_block: 30,
 	fibers: 20,
 	//proxies: []
