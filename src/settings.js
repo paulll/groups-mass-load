6f222074f2c2da5bd4668ef52bd917d87713e039
paulll <paulll@derpy.ru>
2019-09-18 19:43:49 +0300

    add default backend url

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	redis_url: 'redis://localhost',
+	redis_url: 'redis://:UWfQuFUq6YKxyrNqGHKuCRovhAWma3AD@redis-11760.c135.eu-central-1-1.ec2.cloud.redislabs.com:11760',
 	redis_key: 'gml:current_user',
 	output: path.join(__dirname, '../output'),
 	last_block_file: path.join(__dirname, '../.last_block'),
